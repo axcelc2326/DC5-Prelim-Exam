f3c4ae7bcbd8c145e89cbab086a3bb1e5c39b237
axcelc2326 <axcelc2326@gmail.com>
2025-01-30 16:11:22 +0800

    first commit

---
+++ resources/js/Pages/Categories/Index.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { Link } from "@inertiajs/vue3";
+defineProps({
+    categories: {
+        type: Array,
+        required: true,
+    },
+});
+</script>
+
+<template>
+    <div class="container mx-auto py-3 min-h-screen w-full flex justify-center">
+        <div v-if="categories.length === 0">
+            <p>No categories found.</p>
+        </div>
+
+        <div v-else class="">
+            <h1 class="text-4xl font-semibol text-center my-2">Categories</h1>
+            <table
+                class="table-fixed border border-collapse border-gray-400 w-[60vw]"
+            >
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Description</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="category in categories" :key="category.id">
+                        <td>{{ category.name }}</td>
+                        <td>{{ category.description }}</td>
+                    </tr>
+                </tbody>
+            </table>
+
+            <div class="flex justify-center mt-4">
+                <Link
+                    href="/products"
+                    class="text-base hover:text-sky-600 hover:underline duration-100"
+                    >Go to Products</Link
+                >
+            </div>
+        </div>
+    </div>
+</template>
---
+++ resources/js/Pages/Products/Index.vue
@@ -0,0 +1,48 @@
+<script setup>
+import { Link } from "@inertiajs/vue3";
+
+defineProps({
+    products: {
+        type: Array,
+        required: true,
+    },
+    categories: {
+        type: Array,
+        required: true,
+    },
+    selectedCategory: {
+        type: String,
+        required: false,
+    },
+});
+</script>
+
+<template>
+    <div class="flex">
+        <div class="w-[20%] shadow-lg px-2 pt-3 min-h-screen">
+            <nav class="flex flex-col gap-5">
+                <h1 class="text-center text-2xl font-semibold">Categories</h1>
+                <ul v-for="category in categories" :key="category.id">
+                    <li
+                        class="p-2 hover:bg-gray-300 rounded-r-lg duration-100 cursor-pointer"
+                    >
+                        <Link :href="'/products/' + category.id">{{
+                            category.name
+                        }}</Link>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+
+        <div class="w-3/4 p-5">
+            <h1 class="text-4xl font-semibold text-center">
+                {{ selectedCategory ? selectedCategory.name : "All Products" }}
+            </h1>
+            <ul>
+                <li v-for="product in products" :key="product.id">
+                    {{ product.name }}
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
