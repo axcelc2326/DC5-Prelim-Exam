a978b7a8a309f8cae9524724ee6719fa3060874c
axcelc2326 <axcelc2326@gmail.com>
2025-02-12 14:54:35 +0800

    updated

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,7 @@ use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
 
 
-Route::get('/', function () {
-    return inertia('Main');
-});
-
+Route::get('/', function () {return inertia('Main');});
 Route::get('categories', [CategoryController::class, 'index']);
 Route::get('products', [ProductController::class, 'index']);
 Route::get('products/{category}', [ProductController::class, 'index']);
